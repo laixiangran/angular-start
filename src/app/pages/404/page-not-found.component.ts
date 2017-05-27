8fb3d99ec866cb48f7bb911423d3df58a9af2446
laixiangran <laixiangran@163.com>
2017-05-27 12:50:33 +0800

    修改404

--- src/app/pages/404/page-not-found.component.ts
+++ src/app/pages/404/page-not-found.component.ts
@@ -1,36 +1,27 @@
 /**
  * Created by Hllinc on 2016-10-28 12:52.
  */
-import {Component, OnInit} from '@angular/core';
-import {setTimeout} from "timers";
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
     templateUrl: './page-not-found.component.html',
     styleUrls: ['./page-not-found.component.scss']
 })
 
-export class PageNotFoundComponent implements OnInit{
-    constructor(){
+export class PageNotFoundComponent {
+
+    constructor(public router: Router) {
 
     }
-    ngOnInit(){
-        // let timeOutId = window.setTimeout(() => {
-        //     window.clearTimeout(timeOutId);
-        //     history.back();
-        // } ,5000)
-    }
-    pathname: string = location.pathname;
-    href : string = this.pathname.split('/')[0]+'/frame/home';
-
-    beforeUrl : string = document.referrer;
 
     //返回首页
-    rtnHome () {
-        location.href = this.href;
+    goHome() {
+        this.router.navigate(['frame/home']);
     }
 
     //刷新页面
-    refreshPage () {
+    refresh() {
         location.reload();
     }
 }
