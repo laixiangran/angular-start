2f62301837edf3daaf5be81a88d76c076078d862
laixiangran <laixiangran@163.com>
2017-06-18 00:30:38 +0800

    学习ngTemplateOutlet及ViewContainerRef用法

--- src/app/pages/frame/pages/home/home.component.ts
+++ src/app/pages/frame/pages/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component, EmbeddedViewRef, OnDestroy, OnInit, TemplateRef, ViewChild,
+	ViewContainerRef
+} from '@angular/core';
 import { HomeService } from "./home.service";
 
 @Component({
@@ -7,10 +10,36 @@ import { HomeService } from "./home.service";
     providers: [HomeService]
 })
 
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+	myContext: any = {$implicit: 'word', localSk: 'Svet2'};
+	view: EmbeddedViewRef<any>;
+	view2: EmbeddedViewRef<any>;
+	view3: EmbeddedViewRef<any>;
+	@ViewChild('greet') templateRef_greet: TemplateRef<any>;
+	@ViewChild('eng') templateRef_eng: TemplateRef<any>;
+	@ViewChild('svk') templateRef_svk: TemplateRef<any>;
 
-    constructor() {}
+    constructor(public viewContainer: ViewContainerRef) {
+	}
 
-    ngOnInit() {}
+	ngOnInit() {
+		this.view = this.viewContainer.createEmbeddedView(this.templateRef_greet, {
+			$implicit: 'greet'
+		});
+		this.view2 = this.viewContainer.createEmbeddedView(this.templateRef_eng, {
+			$implicit: 'eng'
+		});
+		this.view3 = this.viewContainer.createEmbeddedView(this.templateRef_svk, {
+			$implicit: 'svk',
+			localSk: 'Svet'
+		});
+	}
 
+	ngOnDestroy() {
+		this.view.destroy();
+	}
+
+	changeView() {
+    	this.viewContainer.insert(this.view3, 1);
+	}
 }
