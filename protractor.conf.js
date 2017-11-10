c297751f4324ee1ad3ebb850f430ca123eca66fc
laixiangran <laixiangran@163.com>
2017-11-10 17:00:35 +0800

    新增firefox浏览器

--- protractor.conf.js
+++ protractor.conf.js
@@ -8,9 +8,17 @@ exports.config = {
     specs: [
         './e2e/**/*.e2e-spec.ts'
     ],
-    capabilities: {
-        'browserName': 'chrome'
-    },
+    // capabilities: {
+    //     'browserName': 'chrome'
+    // },
+    multiCapabilities: [
+        {
+            'browserName': 'firefox'
+        },
+        {
+            'browserName': 'chrome'
+        }
+    ],
     directConnect: true,
     baseUrl: 'http://localhost:4200/',
     framework: 'jasmine',
