34d6fc7043b8f499b36ee0cb3bf79ad2a7c5a69c
laixiangran <laixiangran@163.com>
2017-11-15 23:29:29 +0800

    新增cesium-typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,5 @@
-// cesium
-/// <reference path="../node_modules/cesium-typings/index.d.ts" />
+// Typescript definition for Cesium
+/// <reference path="../node_modules/ngx-cesium/src/types/index.d.ts" />
 
 /* SystemJS module definition */
 declare var module: NodeModule;
