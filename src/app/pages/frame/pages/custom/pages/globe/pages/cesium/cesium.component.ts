88bdc17c3c002f3460dffe344b56d37fa1f09681
laixiangran <laixiangran@163.com>
2017-11-17 19:07:22 +0800

    cesium练习

--- src/app/pages/frame/pages/custom/pages/globe/pages/cesium/cesium.component.ts
+++ src/app/pages/frame/pages/custom/pages/globe/pages/cesium/cesium.component.ts
@@ -23,7 +23,7 @@ export class CesiumComponent implements OnInit {
 		const viewer: Viewer = $event;
 
 		//////////////////////////////////////////////////////////////////////////
-		// Loading Imagery
+		// Loading Imagery 加载影像
 		//////////////////////////////////////////////////////////////////////////
 
 		// Add Bing imagery
@@ -34,7 +34,7 @@ export class CesiumComponent implements OnInit {
 		}));
 
 		//////////////////////////////////////////////////////////////////////////
-		// Loading Terrain
+		// Loading Terrain 加载高程
 		//////////////////////////////////////////////////////////////////////////
 
 		// Load STK World Terrain
@@ -43,9 +43,50 @@ export class CesiumComponent implements OnInit {
 			requestWaterMask: true,
 			requestVertexNormals: true
 		});
-		// Enable depth testing so things behind the terrain disappear.
 		// 确保地形背后的物体被正确地遮挡。只有最前面的对象才可见。
 		viewer.scene.globe.depthTestAgainstTerrain = true;
+
+		//////////////////////////////////////////////////////////////////////////
+		// Configuring the Scene 场景设置
+		//////////////////////////////////////////////////////////////////////////
+
+		// 基于太阳/月球位置的照明
+		viewer.scene.globe.enableLighting = true;
+
+		// Create an initial camera view
+		const initialPosition = Cesium.Cartesian3.fromDegrees(-73.998114468289017509, 40.674512895646692812, 2631.082799425431);
+		const initialOrientation = Cesium.HeadingPitchRoll.fromDegrees(7.1077496389876024807, -31.987223091598949054, 0.025883251314954971306);
+		const homeCameraView = {
+			destination: initialPosition,
+			orientation: {
+				heading: initialOrientation.heading,
+				pitch: initialOrientation.pitch,
+				roll: initialOrientation.roll
+			}
+		};
+		// Set the initial view
+		viewer.scene.camera.setView(homeCameraView);
+
+		// Add some camera flight animation options
+		homeCameraView['duration'] = 2.0;
+		homeCameraView['maximumHeight'] = 2000;
+		homeCameraView['pitchAdjustHeight'] = 2000;
+		homeCameraView['endTransform'] = Cesium.Matrix4.IDENTITY;
+		// Override the default home button
+		viewer.homeButton.viewModel.command.beforeExecute.addEventListener(function (e) {
+			e.cancel = true;
+			viewer.scene.camera.flyTo(homeCameraView);
+		});
+
+		// Set up clock and timeline.
+		viewer.clock.shouldAnimate = true; // default
+		viewer.clock.startTime = Cesium.JulianDate.fromIso8601('2017-07-11T16:00:00Z');
+		viewer.clock.stopTime = Cesium.JulianDate.fromIso8601('2017-07-11T16:20:00Z');
+		viewer.clock.currentTime = Cesium.JulianDate.fromIso8601('2017-07-11T16:00:00Z');
+		viewer.clock.multiplier = 2; // sets a speedup
+		viewer.clock.clockStep = Cesium.ClockStep.SYSTEM_CLOCK_MULTIPLIER; // tick computation mode
+		viewer.clock.clockRange = Cesium.ClockRange.LOOP_STOP; // loop at the end
+		viewer.timeline.zoomTo(viewer.clock.startTime, viewer.clock.stopTime); // set visible range
 	}
 }
 
