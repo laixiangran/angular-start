681e52af7e7d1d11ccf4b94394c6ccc62c1352fd
laixiangran <laixiangran@163.com>
2017-12-06 16:10:29 +0800

    试用新版本e-ngx-cesium

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,6 @@ export const environment = {
 	domain: 'http://rapapi.org/mockjsdata/26879',
 	mockDomain: '',
 	tokenName: 'START_URMS_LOGIN_TOKEN',
-	appDownloadUrl: 'http://rapapi.org/mockjsdata/26879/appVersionAction/disposeScanCode'
+	appDownloadUrl: 'http://rapapi.org/mockjsdata/26879/appVersionAction/disposeScanCode',
+	cesiumBaseUrl: '/assets/scripts/cesium'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,5 +10,6 @@ export const environment = {
 	domain: '/mockjsdata/26879', // 请求的域名（请根据实际项目进行修改）
 	mockDomain: '/mockjsdata/26879', // 模拟请求的域名（请根据实际项目进行修改）
 	tokenName: 'START_URMS_LOGIN_TOKEN', // 令牌名称（请根据实际项目进行修改，规则：xxx_URMS_LOGIN_TOKEN）
-	appDownloadUrl: 'http://rapapi.org/mockjsdata/26879/appVersionAction/disposeScanCode' // app下载路径
+	appDownloadUrl: 'http://rapapi.org/mockjsdata/26879/appVersionAction/disposeScanCode', // app下载路径
+	cesiumBaseUrl: '/assets/scripts/cesium' // cesium的请求基础路径
 };
