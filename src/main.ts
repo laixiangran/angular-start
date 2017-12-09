484905084a965f72492b8835d5a0672e75b36578
laixiangran <laixiangran@163.com>
2017-12-09 22:33:13 +0800

    完善cesium示例

--- src/main.ts
+++ src/main.ts
@@ -8,23 +8,26 @@ import { hmrBootstrap } from './hmr';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// 判断是否生产环境
-if (environment.production) {
-	enableProdMode();
-}
-
-// 判断是否启用模块热更新
 const bootstrap = () => {
 	window['CESIUM_BASE_URL'] = environment.cesiumBaseUrl;
 	return platformBrowserDynamic().bootstrapModule(AppModule);
 };
-if (environment.hmr) {
-	if (module['hot']) {
-		hmrBootstrap(module, bootstrap);
-	} else {
-		console.error('webpack-dev-server没有启用HMR！');
-		console.log('你确定在ng serve中设置了--hmr属性？');
-	}
-} else {
+
+// 判断是否生产环境
+if (environment.production) {
+	enableProdMode();
 	bootstrap();
+} else {
+	// 判断是否启用模块热更新
+	if (environment.hmr) {
+		if (module['hot']) {
+			hmrBootstrap(module, bootstrap);
+		} else {
+			console.error('webpack-dev-server没有启用HMR！你确定在ng serve中设置了--hmr属性？');
+		}
+	} else {
+		bootstrap();
+	}
 }
+
+
