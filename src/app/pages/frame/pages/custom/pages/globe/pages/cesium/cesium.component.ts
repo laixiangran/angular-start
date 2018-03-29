16cf1c506f899f26026a6b2b4856dcbcce562abd
laixiangran <laixiangran@163.com>
2018-03-29 22:57:00 +0800

    优化代码

--- src/app/pages/frame/pages/custom/pages/globe/pages/cesium/cesium.component.ts
+++ src/app/pages/frame/pages/custom/pages/globe/pages/cesium/cesium.component.ts
@@ -15,29 +15,14 @@ export class CesiumComponent {
 	viewer: Viewer;
 	scene: Scene;
 	globe: Globe;
-	contrastImageryProviders: ImageryProvider[] = [];
-	enableRollerShutters: boolean = true;
 
 	constructor() {
-		this.viewerOptions = {
-			vrButton: true // 启用VR模式
-		};
-		this.contrastImageryProviders = [
-			new TiandituImageryProvider(TiandituMapsStyle.VEC_C),
-			new TiandituImageryProvider(TiandituMapsStyle.TER_C),
-			new TiandituImageryProvider(TiandituMapsStyle.CVA_C),
-			new TiandituImageryProvider(TiandituMapsStyle.CTA_C)
-		];
 	}
 
 	onViewerReady(evt: any) {
 		this.viewer = evt.viewer;
 		this.scene = evt.scene;
 		this.globe = evt.globe;
-	}
-
-	onSliderChange(evt: any) {
-		console.log(evt);
 	}
 }
 
--- src/app/pages/frame/pages/custom/pages/home/home.component.ts
+++ src/app/pages/frame/pages/custom/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from './home.service';
-import { TransformService } from '../../../../../../services/transform.service';
 
 @Component({
 	templateUrl: './home.component.html',
@@ -9,28 +8,10 @@ import { TransformService } from '../../../../../../services/transform.service';
 })
 export class HomeComponent implements OnInit {
 	welcomeMessage: string = 'Welcome to Angular World!';
-	inCoord: string;
-	inCoordType: string = '1';
-	inCoordUnit: string = '0';
-	outCoord: string;
-	outCoordType: string = '0';
-	outCoordUnit: string = '0';
 
-	constructor(public transformService: TransformService) {
+	constructor() {
 	}
 
 	ngOnInit() {
-	}
-
-	doConvert() {
-		const inCoordX: number = this.inCoordUnit === '1' ? this.transformService.degree2Decimal(this.inCoord.split(',')[0]) : +this.inCoord.split(',')[0];
-		const inCoordY: number = this.inCoordUnit === '1' ? this.transformService.degree2Decimal(this.inCoord.split(',')[1]) : +this.inCoord.split(',')[1];
-		if (this.inCoordType === '1' && this.outCoordType === '0') {
-			const outCoordObj: { lat: number; lng: number } = this.transformService.gcj2wgs(inCoordY, inCoordX);
-			console.log(outCoordObj);
-			const outCoordObjX: string = this.outCoordUnit === '1' ? this.transformService.decimal2Degree(outCoordObj.lng) : outCoordObj.lng.toFixed(6);
-			const outCoordObjY: string = this.outCoordUnit === '1' ? this.transformService.decimal2Degree(outCoordObj.lat) : outCoordObj.lat.toFixed(6);
-			this.outCoord = [outCoordObjX, outCoordObjY].join(',');
-		}
 	}
 }
